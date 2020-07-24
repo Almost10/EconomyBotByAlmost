1eb7f58f402bf5106008a38a6d5a4ff72bcbe616
TbagMaster69 <snowsweater@hotmail.com>
2020-07-24 10:59:26 -0400

    Update adminpay.js

--- commands/adminpay.js
+++ commands/adminpay.js
@@ -12,7 +12,7 @@ const Data = require("../models/data.js");
 const data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) => {
-    if(message.author.role != "445706307698294786") return message.reply("Low ranking members cannot perform this command!");
+    if(!message.member.roles.find(r => r.name === "Leaders/Representatives")) return message.reply("Low ranking members cannot perform this command!");
 
     let user = message.mentions.members.first() || bot.users.cache.get(args[0]);
     if(!user) return message.reply("I couldn't find that person");
